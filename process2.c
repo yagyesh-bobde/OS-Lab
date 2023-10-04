33df165976a00ceaf1fcebbb1e828851072c140f
Yagyesh Bobde <90238742+yagyesh-bobde@users.noreply.github.com>
2023-10-04 13:16:11 +0530

    Create process2.c

---
+++ process2.c
@@ -0,0 +1,70 @@
+// without arrival time
+#include <stdio.h>
+
+struct Process {
+    int id;
+    int burst_time;
+    int waiting_time;
+    int turnaround_time;
+};
+
+void calculateWaitingTime(struct Process processes[], int n) {
+    int total_waiting_time = 0;
+    processes[0].waiting_time = 0; // The first process has 0 waiting time
+
+    for (int i = 1; i < n; i++) {
+        // Calculate waiting time for process i
+        processes[i].waiting_time = processes[i - 1].waiting_time + processes[i - 1].burst_time;
+        total_waiting_time += processes[i].waiting_time;
+    }
+}
+
+void calculateTurnaroundTime(struct Process processes[], int n) {
+    for (int i = 0; i < n; i++) {
+        processes[i].turnaround_time = processes[i].waiting_time + processes[i].burst_time;
+    }
+}
+
+void calculateAverageTime(struct Process processes[], int n) {
+    calculateWaitingTime(processes, n);
+    calculateTurnaroundTime(processes, n);
+
+    int total_turnaround_time = 0;
+    int total_waiting_time = 0;
+
+    for (int i = 0; i < n; i++) {
+        total_turnaround_time += processes[i].turnaround_time;
+        total_waiting_time += processes[i].waiting_time;
+    }
+
+    float average_waiting_time = (float)total_waiting_time / n;
+    float average_turnaround_time = (float)total_turnaround_time / n;
+
+    printf("Process\tBurst Time\tWaiting Time\tTurnaround Time\n");
+
+    for (int i = 0; i < n; i++) {
+        printf("%d\t%d\t\t%d\t\t%d\n", processes[i].id, processes[i].burst_time,
+               processes[i].waiting_time, processes[i].turnaround_time);
+    }
+
+    printf("Average Waiting Time: %.2f\n", average_waiting_time);
+    printf("Average Turnaround Time: %.2f\n", average_turnaround_time);
+}
+
+int main() {
+    int n;
+    printf("Enter the number of processes: ");
+    scanf("%d", &n);
+
+    struct Process processes[n];
+
+    for (int i = 0; i < n; i++) {
+        processes[i].id = i + 1;
+        printf("Enter burst time for process %d: ", i + 1);
+        scanf("%d", &processes[i].burst_time);
+    }
+
+    calculateAverageTime(processes, n);
+
+    return 0;
+}
